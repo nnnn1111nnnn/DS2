e4c96969da01d438a7fa28b6f7663f3e64528a14
s2526239 <s2526239@vuw.leidenuniv.nl>
2024-04-16 16:34:57 +0200

    .
    w7 p1

--- week7.py
+++ week7.py
@@ -3,9 +3,10 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor, BaggingRegressor
 from nltk.stem.snowball import SnowballStemmer
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 
 stemmer = SnowballStemmer('english')
-
+stemming = input("Do you want to use stemming? (y/n): ")
 df_train = pd.read_csv('train.csv/train.csv', encoding="ISO-8859-1")
 df_test = pd.read_csv('test.csv/test.csv', encoding="ISO-8859-1")
 #df_attr = pd.read_csv('attributes.csv/attributes.csv')
@@ -31,11 +32,13 @@ df_all = df_all.rename(columns={'product_uid ': 'product_uid'})
 #print("Columns in df_pro_desc: ", df_pro_desc.columns)
 
 df_all = pd.merge(df_all, df_pro_desc, how='left', on="product_uid")
-
-df_all['search_term'] = df_all['search_term'].map(lambda x:str_stemmer(x))
-df_all['product_title'] = df_all['product_title'].map(lambda x:str_stemmer(x))
-df_all['product_description'] = df_all['product_description'].map(lambda x:str_stemmer(x))
-
+if stemming == 'y':
+    print("Executing with stemming")
+    df_all['search_term'] = df_all['search_term'].map(lambda x:str_stemmer(x))
+    df_all['product_title'] = df_all['product_title'].map(lambda x:str_stemmer(x))
+    df_all['product_description'] = df_all['product_description'].map(lambda x:str_stemmer(x))
+else:
+     print("Executing without stemming")
 df_all['len_of_query'] = df_all['search_term'].map(lambda x:len(x.split())).astype(np.int64)
 
 df_all['product_info'] = df_all['search_term']+"\t"+df_all['product_title']+"\t"+df_all['product_description']
@@ -46,23 +49,18 @@ df_all['word_in_description'] = df_all['product_info'].map(lambda x:str_common_w
 df_all = df_all.drop(['search_term','product_title','product_description','product_info'],axis=1)
 
 df_train = df_all.iloc[:num_train]
-df_test = df_all.iloc[num_train:]
-id_test = df_test['id']
 
-y_train = df_train['relevance'].values
-X_train = df_train.drop(['id','relevance'],axis=1).values
-X_test = df_test.drop(['id','relevance'],axis=1).values
+# Define the features (X) and target variable (y)
+X = df_train.drop(['id', 'relevance'], axis=1)  # Features
+y = df_train['relevance']  # Target variable
 
-
-
-
+# Split the data into training and testing sets (80% training, 20% testing)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 rf = RandomForestRegressor(n_estimators=15, max_depth=6, random_state=0)
 clf = BaggingRegressor(rf, n_estimators=45, max_samples=0.1, random_state=25)
 clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
 
+rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 
-
-
-
-pd.DataFrame({"id": id_test, "relevance": y_pred}).to_csv('submission_srf.csv',index=False)
+print("Root Mean Squared Error (RMSE):", rmse)
